049fa0d8a9e0402bc5c4e2c3c59f7e2874156843
nguyentrongdoan <nguyendoan206@gmail.com>
2020-02-17 16:41:26 +0700

    Test

---
+++ src/minToArray.java
@@ -0,0 +1,19 @@
+public class minToArray {
+    public static void main(String[] args) {
+        int []arr = {4, 12, 7, 8, 1, 6, 9};
+        int index = minToArray(arr);
+        System.out.println("The smallest element in the array is: " + arr[index]);
+    }
+    public static int minToArray(int []array){
+        int min = array[0];
+        int index = 0;
+
+        for (int i = 1; i < array.length; i++){
+            if (array[i] < min){
+                min = array[i];
+                index = i;
+            }
+        }
+        return index;
+    }
+}
